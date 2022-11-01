84151b9cbae044a7420f7ad85594b09f062fbdd0
Jayden Houghton <109559841+Jayden-Htn@users.noreply.github.com>
2022-11-02 09:26:58 +1300

    create backup batch file

---
+++ JTPM-Backup.bat
@@ -0,0 +1,4 @@
+:: JTPM DATABASE BACKUP SCRIPT
+::mysqldump path, account, password, database, backup file name
+
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -u root -proot123 JTPM > jtpm_backup.sql
